ec899c58e1567979569914f05d43b53ebe7911b9
lkuklaexert <lkukla.exertis@gmail.com>
2026-05-22 17:53:56 +0100

    Fixing today scroll check box

--- src/App.jsx
+++ src/App.jsx
@@ -605,7 +605,7 @@ const visibleEmployees = useMemo(() => {
     const approxColumnWidth = 35;
     const staticColumnsWidth = 650;
   
-    el.scrollLeft = Math.max(0, (dayIndex * approxColumnWidth) + 298);
+    el.scrollLeft = Math.max(0, (dayIndex * approxColumnWidth) + 368);
   }
 
   if (!session) {
